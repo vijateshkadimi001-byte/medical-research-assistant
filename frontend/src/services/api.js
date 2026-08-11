a58d5b374bfc2a18310f36ae59003437a335237b
Vijatesh K <Vijateshkadimi001@gmail.com>
2026-08-11 17:10:18 +0530

    Connect frontend to Railway backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import { getToken, removeToken } from "../utils/storage";
 
 const api = axios.create({
-  baseURL: "http://127.0.0.1:8000",
+  baseURL: import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
